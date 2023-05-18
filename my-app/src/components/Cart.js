793449331db6fa696a540d7b8c9c9600d1d2bf94
Jost Christopher <jostchristopher44@gmail.com>
2023-05-18 19:13:54 +0200

    lalala

--- my-app/src/components/Cart.js
+++ my-app/src/components/Cart.js
@@ -1,35 +1,60 @@
+import '../styles/Cart.css';
+
+function Cart() {
+    return (
+        <div className="kasa-cart">
+            <h2>la vida loca</h2>
+        </div>
+    );
+}
+
+export default Cart;
+
+// import { useParams } from 'react-router-dom';
 // import '../styles/Cart.css';
+// import { useEffect, useState } from 'react';
 
 // function Cart() {
+//     const { title } = useParams();
+//     const [demdem, setDemdem] = useState({});
+//     const
+
+//     useEffect(() => {
+//         fetch(`/logements.json`)
+//             .then((response) => response.json())
+//             .then((response) => console.log(response, 1111))
+//             .catch((error) => console.log(error));
+//     }, []);
+//     console.log(demdem, 22222222);
 //     return (
 //         <div className="kasa-cart">
-//             <h2>la vida loca</h2>
+//             <h2 className="cartTitle">{title}</h2>
 //         </div>
 //     );
 // }
 
 // export default Cart;
 
-import { useParams } from 'react-router-dom';
-import '../styles/Cart.css';
-import { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import '../styles/Cart.css';
+// import { useEffect, useState } from 'react';
 
-function Cart() {
-    const { title } = useParams();
-    const [data] = useState({});
+// function Survey() {
+//     const title = useParams();
+//     const [surveyData, setSurveyData] = useState({});
 
-    useEffect(() => {
-        fetch(`/logements.json`)
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.log(error));
-    }, []);
+//     useEffect(() => {
+//         fetch(`/logements.json`)
+//             .then((response) => response.json())
+//             .then((surveyData) => setSurveyData(surveyData))
+//             .catch((error) => console.log(error));
+//     }, []);
+//     console.log(surveyData);
+//     return (
+//         <div>
+//             <h2>{surveyData[0].title}</h2>
+//         </div>
+//     );
+// }
 
-    return (
-        <div className="kasa-cart">
-            <h2 className="cartTitle">{title}</h2>
-        </div>
-    );
-}
-
-export default Cart;
+// export default Survey;
